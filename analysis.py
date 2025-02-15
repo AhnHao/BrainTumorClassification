a26488252286718b22031d64dcceb8d7ab15f6bc
AhnHao <haotrananh2@gmail.com>
2025-02-15 09:42:10 +0700

    Analysis and create Dataset

---
+++ analysis.py
@@ -0,0 +1,73 @@
+import os
+import random
+import matplotlib.pyplot as plt
+from PIL import Image
+
+# Đường dẫn đến tập dữ liệu
+train_dir = "dataset/Training"
+test_dir = "dataset/Testing"
+
+# Đếm số lượng ảnh trong mỗi lớp
+def count_images(directory):
+    classes = os.listdir(directory)
+    class_counts = {cls: len(os.listdir(os.path.join(directory, cls))) for cls in classes}
+    return class_counts
+
+# Đếm số ảnh trong tập train & test
+train_counts = count_images(train_dir)
+test_counts = count_images(test_dir)
+
+# Tổng số ảnh trong mỗi tập
+total_train = sum(train_counts.values())
+total_test = sum(test_counts.values())
+
+# Vẽ biểu đồ phân bố số lượng ảnh trong tập huấn luyện
+def plot_image_distribution(data_counts, title):
+    plt.figure(figsize=(8, 6))
+    bars = plt.bar(data_counts.keys(), data_counts.values(), color=['blue', 'red', 'green', 'yellow'])
+    # Thêm nhãn số lượng ảnh lên mỗi cột
+    for bar in bars:
+        height = bar.get_height()
+        plt.text(bar.get_x() + bar.get_width()/2, height, str(height), ha='center', va='bottom', fontsize=12, color='black')
+    plt.ylim(0, max(data_counts.values()) * 1.1)
+    plt.xlabel("Lớp")
+    plt.ylabel("Số lượng ảnh")
+    plt.title("Phân bố số lượng ảnh trong tập " + title)
+    plt.show()
+
+plot_image_distribution(train_counts, "huấn luyện")
+plot_image_distribution(test_counts, "kiểm tra")
+
+# Vẽ biểu đồ tròn (tỷ lệ giữa tập train & test)
+plt.figure(figsize=(6, 6))
+labels = ['Train', 'Test']
+sizes = [total_train, total_test]
+colors = ['blue', 'red']
+
+plt.pie(sizes, labels=labels, autopct='%1.1f%%', colors=colors, startangle=90, wedgeprops={'edgecolor': 'black'})
+plt.title("Tỷ lệ số lượng ảnh giữa tập huấn luyện và tập kiểm tra")
+plt.show()
+
+# Hiển thị ảnh
+def show_random_images(directory, num_images=8):
+    classes = os.listdir(directory)
+    plt.figure(figsize=(12, 6))
+
+    for i in range(num_images):
+        cls = random.choice(classes)  # Chọn ngẫu nhiên lớp
+        class_folder = os.path.join(directory, cls)
+        img_name = random.choice(os.listdir(class_folder))  # Chọn ngẫu nhiên ảnh
+        img_path = os.path.join(class_folder, img_name)
+
+        image = Image.open(img_path)
+
+        plt.subplot(2, 4, i + 1)
+        plt.imshow(image)
+        plt.title(cls)
+        plt.axis("off")
+
+    plt.show()
+
+# Hiển thị ảnh từ tập huấn luyện
+show_random_images(train_dir)
+
---
+++ dataset.py
@@ -0,0 +1,57 @@
+import os
+import torch
+from torchvision import transforms
+from torch.utils.data import Dataset, DataLoader
+from PIL import Image
+
+class BrainTumorDataset(Dataset):
+    def __init__(self, root_dir, transform=None):
+        self.root_dir = root_dir
+        self.transform = transform
+        self.classes = os.listdir(root_dir)
+        self.image_paths = []
+        self.labels = []
+
+        for label, cls in enumerate(self.classes):
+            cls_folder = os.path.join(root_dir, cls)
+            for img_name in os.listdir(cls_folder):
+                self.image_paths.append(os.path.join(cls_folder, img_name))
+                self.labels.append(label)
+
+    def __len__(self):
+        return len(self.image_paths)
+
+    def __getitem__(self, idx):
+        img_path = self.image_paths[idx]
+        image = Image.open(img_path).convert("RGB")
+        label = self.labels[idx]
+
+        if self.transform:
+            image = self.transform(image)
+
+        return image, label
+
+# Tạo transform
+train_transform = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.RandomHorizontalFlip(p=0.5),
+    transforms.RandomRotation(10),
+    transforms.RandomAffine(degrees=0, translate=(0.1, 0.1)),
+    transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+])
+
+test_transform = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+])
+
+# Thay đổi đường dẫn dataset tương ứng
+train_dataset = BrainTumorDataset(root_dir="dataset/Training", transform=train_transform)
+test_dataset = BrainTumorDataset(root_dir="dataset/Testing", transform=test_transform)
+
+# Tạo DataLoader
+train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
+test_loader = DataLoader(test_dataset, batch_size=32, shuffle=False)
